63c045c7251afbc74fdf3cef2fdf06838f81e700
logancancode <137139714+logancancode@users.noreply.github.com>
2024-04-03 08:19:40 -0400

    Update Rock-Paper-Scissors.js game logic

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -1,38 +1,33 @@
-const playerPrompt = "Rock, Paper, or Scissors?";
-const rock_paper_scissors_selection = ["Rock", "Paper", "Scissors"]
-let win = "You win! ";
-let lose = "You lose. Better luck next time.";
+const playerPrompt = "Rock, Paper, or Scissors?" ;
+const choice = [null, "rock", "paper", "scissors"];
+const winMessage = "You win!" ; 
+const lossMessage = "You lose;" ;
+let playerScore = 0;
+let computerScore = 0;
 
-let randomNum = Math.floor(Math.random()*9)%3;
+const playerSelection = prompt(playerPrompt).toLowerCase();
+console.log("You chose " + playerSelection + ".");
 
-function getComputerChoice(randomNum) {
-    return rock_paper_scissors_selection[randomNum];
-}
+const computerSelection = choice[Math.floor(Math.random() * 3) +1];
+console.log("The computer chose" + " " + computerSelection + ".");
 
-function getPlayerSelection() {
-    prompt(playerPrompt).toLowerCase();
-}
-
-
-let computerSelection = getComputerChoice(randomNum);
-let playerSelection = getPlayerSelection();
-let roundOutcome = "if/elses failed";
-function gameRound(playerSelection, computerSelection) {
-   // if(playerSelection == computerSelection) {
-   //     roundOutcome = "It's a draw. Try again!"; }
-   /* else */ if (playerSelection == "rock" && computerSelection == "Scissors") {
-        roundOutcome = win + "Rock crushes Scissors."; }
-    else if (playerSelection == "scissors" && computerSelection == "Paper") {
-        roundOutcome = win + "Scissors cuts Paper."; }
-    else if (playerSelection == "paper" && computerSelection == "Rock") {
-        roundOutcome = win + "Paper covers Rock."; }
-    else roundOutcome = "You lose. Better luck next time.";
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        return winMessage + " Rock breaks scissors."}
+    else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        return lossMessage + " paper covers rock. Better luck next time!" }
+    else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        return winMessage + " Paper covers rock."}
+    else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        return lossMessage + " scissors cuts paper. Better luck next time!"}
+    else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        return winMessage + " Scissors cuts paper."}
+    else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        return lossMessage + " rock breaks scissors. Better luck next time!" }
+    else if (playerSelection == computerSelection) {
+        return "It's a draw! Ready to play again?"}
+    else return "You didn't enter a valid choice. Try again!";
     }
-function playRound() {
-    prompt(playerPrompt).toLowerCase()
-    getComputerChoice(randomNum);
-    gameRound(playerSelection, computerSelection);
-}
 
-playRound();
-console.log(roundOutcome);
+let roundMessage = (playRound(playerSelection, computerSelection))
+console.log(roundMessage);
